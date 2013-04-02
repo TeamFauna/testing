4736be2b14955d331413577df79ec9dffaafb7c7
Fravic <fravicf@gmail.com>
2013-04-02 16:43:11 -0400

    stuff

--- pi/pipair.java
+++ pi/pipair.java
@@ -2,39 +2,59 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Collections;
 
-class Pair {
-    String aName;
-    String bName;
-    int support;
-    float confidence;
-
-    public String toString() {
-        return "TODO";
-    }
-}
-
 class Pipair {
 
-    static final int T_SUPPORT = 3;
-    static final float T_CONFIDENCE = 0.65f;
+    class Pair {
+        String aName;
+        String bName;
+        int support;
+        float confidence;
 
-    public void run() {
-        ArrayList<Pair> results = new ArrayList<Pair>();
+        public String toString() {
+            return "TODO";
+        }
+    }
+
+    class CallGraph {
+        public ArrayList<Pair> getPairs() {
+            return new ArrayList<Pair>();
+        }
+    }
+
+    int tSupport = 3;
+    float tConfidence = 0.65f;
+
+    public void run(String cgFile) {
+        CallGraph cg = new CallGraph();
+        ArrayList<Pair> results = cg.getPairs();
         printResults(results);
+        System.out.println(tSupport);
+        System.out.println(tConfidence);
     }
 
     public void printResults(ArrayList<Pair> results) {
         Enumeration e = Collections.enumeration(results);
         while (e.hasMoreElements()) {
             Pair p = (Pair)e.nextElement();
-            if (p.support > T_SUPPORT && p.confidence > T_CONFIDENCE) {
+            if (p.support > tSupport && p.confidence > tConfidence) {
                 System.out.println("bug: " + e.nextElement());
             }
         }
     }
 
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Usage: ./pipair <bitcode file> <T SUPPORT> <T CONFIDENCE>,");
+            System.exit(0);
+        }
+
         Pipair prog = new Pipair();
-        prog.run();
+        if (args.length >= 2) {
+            prog.tSupport = Integer.parseInt(args[1]);
+        }
+        if (args.length >= 3) {
+            prog.tConfidence = Float.parseFloat(args[2]);
+        }
+        prog.run(args[0]);
     }
 }
