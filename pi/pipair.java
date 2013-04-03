301111f26bf40a738d1a093c50b53bcd9aaa918d
Fravic <fravicf@gmail.com>
2013-04-02 23:41:55 -0400

    All the things

--- pi/pipair.java
+++ pi/pipair.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.io.IOException;
 import java.util.Hashtable;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 
 class Pipair {
 
@@ -19,6 +21,10 @@ class Pipair {
         public Pair(String aName, String bName) {
             _aName = aName;
             _bName = bName;
+        }
+
+        public String getTarget() {
+            return _bName;
         }
 
         public void strengthen() {
@@ -44,12 +50,10 @@ class Pipair {
 
     class Violation {
         private String _caller;
-        private String _violator;
         private Pair _pair;
 
-        public Violation(String caller, String violator, Pair pair) {
+        public Violation(String caller, Pair pair) {
             _caller = caller;
-            _violator = violator;
             _pair = pair;
         }
 
@@ -67,47 +71,112 @@ class Pipair {
         return new Hashtable<String,ArrayList<String>>();
     }
 
-    public Hashtable<String,ArrayList<Pair>> getInvariantPairs(Hashtable<String,ArrayList<String>> cg) {
-        Hashtable<String,ArrayList<Pair>> pairs = new Hashtable<String,ArrayList<Pair>>();
+    public Hashtable<String,Hashtable<String,Pair>>
+        getInvariantPairs(Hashtable<String,ArrayList<String>> cg) {
+
+        Hashtable<String,Hashtable<String,Pair>> pairs = getAllInvariantPairs(cg);
+        rejectWeakPairs(pairs);
+        return pairs;
+    }
+
+    private Hashtable<String,Hashtable<String,Pair>> getAllInvariantPairs(Hashtable<String,ArrayList<String>> cg) {
+        Hashtable<String,Hashtable<String,Pair>> pairs =
+            new Hashtable<String,Hashtable<String,Pair>>();
 
         Enumeration funcs = cg.elements();
         while (funcs.hasMoreElements()) {
             ArrayList<String> calls = (ArrayList<String>)funcs.nextElement();
-            
-            // Remove duplicates from calls
-            HashSet<String> callSet = new HashSet(calls);
-            calls = new ArrayList(callSet);
+            removeDuplicateCalls(calls);
 
             for (int i = 0; i < calls.size(); i++) {
                 for (int j = i + 1; j < calls.size(); j++) {
-                    // For both possible pairs...
-
-                    // Check for existing pair
-
-                    // If existing pair, increment it
-
-                    // If not, create it
+                    createOrStrengthenPair(pairs, calls.get(i), calls.get(j));
+                    createOrStrengthenPair(pairs, calls.get(j), calls.get(i));
                 }
 
-                ArrayList<Pair> existingPairs = pairs.get(calls.get(i));
+                Hashtable<String,Pair> existingPairs = pairs.get(calls.get(i));
                 for (int k = 0; k < existingPairs.size(); k++) {
                     existingPairs.get(k).weaken();
                 }
             }
         }
-        
+
         return pairs;
     }
 
-    public ArrayList<Violation> getViolations(Hashtable<String,ArrayList<String>> cg,
-                                              Hashtable<String,ArrayList<Pair>> invariants) {
-        //if (p.getSupport() > tSupport && p.getConfidence() > tConfidence) {
-        return new ArrayList<Violation>(); // TODO
+    private void
+        rejectWeakPairs(Hashtable<String,Hashtable<String,Pair>> pairs) {
+
+        Enumeration pairLists = pairs.elements();
+        while (pairLists.hasMoreElements()) {
+            Hashtable<String,Pair> pairList = (Hashtable<String,Pair>)pairLists.nextElement();
+            Enumeration callPairs = pairList.elements();
+            while (callPairs.hasMoreElements()) {
+                Pair p = (Pair)callPairs.nextElement();
+                if (p.getSupport() > tSupport &&
+                    p.getConfidence() > tConfidence) {
+                    pairList.remove(p);
+                }
+            }
+        }
+    }
+
+    private void createOrStrengthenPair(Hashtable<String,Hashtable<String,Pair>>
+                                       pairs,
+                                       String f1, String f2) {
+        Hashtable<String,Pair> funcPairs = pairs.get(f1);
+        if (funcPairs == null) {
+            funcPairs = new Hashtable<String,Pair>();
+            pairs.put(f1, funcPairs);
+        }
+        Pair p = funcPairs.get(f2);
+        if (p == null) {
+            p = new Pair(f1, f2);
+            funcPairs.put(f2, p);
+        }
+        p.strengthen();
+    }
+
+    private void removeDuplicateCalls(ArrayList<String> calls) {
+        HashSet<String> callSet = new HashSet(calls);
+        calls = new ArrayList(callSet);
+    }
+
+    public ArrayList<Violation>
+        getViolations(Hashtable<String,ArrayList<String>> cg,
+                      Hashtable<String,Hashtable<String,Pair>> invariants) {
+
+        ArrayList<Violation> violations = new ArrayList<Violation>();
+
+        HashSet<Map.Entry<String,ArrayList<String>>> cgSet = (HashSet<Map.Entry<String,ArrayList<String>>>)cg.entrySet();
+        Iterator functions = cgSet.iterator();
+        while (functions.hasNext()) {
+            Map.Entry<String,ArrayList<String>> entry = (Map.Entry<String,ArrayList<String>>)functions.next();
+            String functionName = (String)entry.getKey();
+            ArrayList<String> callsL = (ArrayList<String>)entry.getValue();
+            HashSet<String> calls = new HashSet(callsL);
+
+            Iterator i = calls.iterator();
+            while (i.hasNext()) {
+                Hashtable invariantsForCall = invariants.get(i.next());
+                Enumeration pairs = invariantsForCall.elements();
+                while (pairs.hasMoreElements()) {
+                    Pair invariant = (Pair)pairs.nextElement();
+                    if (!calls.contains(invariant.getTarget())) {
+                        violations.add(new Violation(functionName,
+                                                      invariant));
+                    }
+                }
+            }
+        }
+
+        return violations;
     }
 
     public void run(String cgFile) {
         Hashtable<String,ArrayList<String>> cg = parseFile(cgFile);
-        Hashtable<String,ArrayList<Pair>> invariants = getInvariantPairs(cg);
+        Hashtable<String,Hashtable<String,Pair>> invariants =
+            getInvariantPairs(cg);
         ArrayList<Violation> violations = getViolations(cg, invariants);
         printViolations(violations);
     }
