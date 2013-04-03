885ecb94c1c55530ab8055443f581b3e3b5ecbc3
Will Hughes <willhughes01@gmail.com>
2013-04-02 22:26:24 -0400

    df

--- pi/pipair.java
+++ pi/pipair.java
@@ -49,7 +49,7 @@ class Pipair {
                     break;
                   }
                 case(1):
-                  if (line.matches("CS\<0x[1-9a-f]*\> calls function")) {
+                  if (line.matches("CS\\<0x[1-9a-f]*\\> calls function")) {
                     String[] slist = line.split('\'');
                     String func = slist[1];
                     ArrayList<String> curList = table.get(current);
