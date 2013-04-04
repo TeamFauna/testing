a808535073c741fbee89572521eea979cf7140a6
Will Hughes <willhughes01@gmail.com>
2013-04-03 21:17:22 -0400

    Added comments to parser

--- pi/parser.java
+++ pi/parser.java
@@ -15,10 +15,17 @@ import java.text.NumberFormat;
 import java.math.RoundingMode;
 
 class Parser {
+    /* Take a bc file and retrieve the callgraph using the opt command
+     * 
+     * If we want to do interprocedural analysis, expand the callgraph to n levels
+     */
     public static Hashtable<String,ArrayList<String>> parseFile(String fileName, int levels) {
-        Runtime rt = Runtime.getRuntime();
+        // This table will point "Functions" to a list of their "calls"
         Hashtable<String, ArrayList<String>> table = new Hashtable<String,ArrayList<String>>();
+        
+        Runtime rt = Runtime.getRuntime();
         try {
+            //Print the callgraph to stderr, disable stdout because it's all junk
             Process pr = rt.exec("opt -print-callgraph -disable-output " + fileName);
             InputStream st = pr.getErrorStream();
             BufferedReader in = new BufferedReader(new InputStreamReader(st));
@@ -32,7 +39,7 @@ class Parser {
               //System.out.println(line + " " + line.length());
 
               switch (state) {
-                case(1):
+                case(1): //We are in a state that already has a "function" look for the "calls"
                   if (line.matches("(.*)CS<0x[0-9a-f]*> calls function(.*)")) {
                     String[] slist = line.split("\'");
                     String func = slist[1];
@@ -43,7 +50,7 @@ class Parser {
                     //System.out.println(func);
                     break;
                   }
-                case(0):
+                case(0): //Look for a "function"
                   if (line.startsWith("Call graph node for function")) {
                     
                     String[] slist = line.split("\'");
@@ -66,20 +73,22 @@ class Parser {
             
             //If we are expanding functions n levels deep, we do it here
             for (int l = 0; l < levels; l++) {
+              //We need a callgraph that won't change as we expand functions
               Hashtable<String,ArrayList<String>> staticTable = deepCopy(table);
 
               Enumeration funcs = table.elements();
-
+              //Loop through all the functions to expand their calls
               while (funcs.hasMoreElements()) {
                 ArrayList<String> calls = (ArrayList<String>)funcs.nextElement();
-
                 ArrayList<String> originalCalls = (ArrayList<String>)calls.clone();
 
+                //Loop through the calls and expand the functions they contain
                 for (int i = 0; i < originalCalls.size(); i++) {
                   String expandFunc = originalCalls.get(i);
 
                   ArrayList<String> funcsToBeAdded = staticTable.get(expandFunc);
 
+                  //If there are no functions to expand, don't remove the original call from the function
                   if (funcsToBeAdded.size() > 0) {
                     calls.remove(expandFunc);
                   }
